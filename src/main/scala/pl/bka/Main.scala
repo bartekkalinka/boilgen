95dec019ef3a0b1c8b05616a9689eb93ffb0f04e
Bartek Kalinka <bartkali@hotmail.com>
2017-09-02 15:43:09 +0200

    generating anyval case classes

--- src/main/scala/pl/bka/Main.scala
+++ src/main/scala/pl/bka/Main.scala
@@ -1,4 +1,36 @@
 package pl.bka
 
-object Main {
+import scala.meta._
+
+object Main extends App {
+  val input =
+    """
+      |case class Transaction(
+      |  id: UUID,
+      |  value: Long,
+      |  createDate: Instant,
+      |  modifiedDate: Instant,
+      |  cardNumber: String,
+      |  description: String
+      |)
+    """.stripMargin
+
+  def ucFirst(str: String): String = Character.toUpperCase(str.charAt(0)) + str.substring(1)
+
+  val tree = input.parse[Source].get
+  tree match {
+    case source"..$stats" =>
+      stats.head match {
+        case q"..$mods class $tname[..$tparams] ..$ctorMods (...$paramss) extends $template" =>
+          val classDefns = paramss.flatten.map { param =>
+            val caseClassName = Type.Name(tname + ucFirst(param.name.syntax))
+            val typeName = param.decltpe.map(d => d.syntax).getOrElse("String").parse[Type.Arg].get
+            q"case class $caseClassName(value: $typeName) extends AnyVal"
+          }
+          val newSource = source"..$classDefns"
+          println(newSource.syntax)
+        case _ =>
+          println("not a class")
+      }
+  }
 }
