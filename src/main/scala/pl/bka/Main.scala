e0aadd678e08353410fe32aaf216c67e49f87ff9
Bartek Kalinka <bartkali@hotmail.com>
2017-09-03 17:04:50 +0200

    extracting method for anyval class

--- src/main/scala/pl/bka/Main.scala
+++ src/main/scala/pl/bka/Main.scala
@@ -17,16 +17,18 @@ object Main extends App {
 
   def ucFirst(str: String): String = Character.toUpperCase(str.charAt(0)) + str.substring(1)
 
+  def anyValClassForField(mainClassName: Type.Name, field: Term.Param): Defn.Class = {
+    val caseClassName = Type.Name(mainClassName + ucFirst(field.name.syntax))
+    val typeName = field.decltpe.get
+    q"case class $caseClassName(value: $typeName) extends AnyVal"
+  }
+
   val tree = input.parse[Source].get
   tree match {
     case source"..$stats" =>
       stats.head match {
         case q"..$mods class $tname[..$tparams] ..$ctorMods (...$paramss) extends $template" =>
-          val classDefns = paramss.flatten.map { param =>
-            val caseClassName = Type.Name(tname + ucFirst(param.name.syntax))
-            val typeName = param.decltpe.get
-            q"case class $caseClassName(value: $typeName) extends AnyVal"
-          }
+          val classDefns = paramss.flatten.map(anyValClassForField(tname, _))
           val newSource = source"..$classDefns"
           println(newSource.syntax)
         case _ =>
