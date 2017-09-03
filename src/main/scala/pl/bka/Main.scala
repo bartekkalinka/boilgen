3b190134a3bd7e866101d435f0f764dd42afa9c5
Bartek Kalinka <bartkali@hotmail.com>
2017-09-03 17:25:54 +0200

    generating replaced fields types class

--- src/main/scala/pl/bka/Main.scala
+++ src/main/scala/pl/bka/Main.scala
@@ -17,12 +17,13 @@ object Main extends App {
 
   def ucFirst(str: String): String = Character.toUpperCase(str.charAt(0)) + str.substring(1)
 
-  case class ClassDefnWithName(name: Type.Name, defn: Defn.Class)
+  case class ClassDefnWithName(name: Type.Name, defn: Defn.Class, originalFieldName: Term.Param.Name)
 
   def anyValClassForField(mainClassName: Type.Name, field: Term.Param): ClassDefnWithName = {
     val caseClassName = Type.Name(mainClassName + ucFirst(field.name.syntax))
-    val typeName = field.decltpe.get
-    ClassDefnWithName(caseClassName, q"case class $caseClassName(value: $typeName) extends AnyVal")
+    val typeName = field.decltpe.getOrElse(Type.Name("String"))
+    val anyValClass = q"case class $caseClassName(value: $typeName) extends AnyVal"
+    ClassDefnWithName(caseClassName, anyValClass, field.name)
   }
 
   val tree = input.parse[Source].get
@@ -31,8 +32,13 @@ object Main extends App {
       stats.head match {
         case q"..$mods class $tname[..$tparams] ..$ctorMods (...$paramss) extends $template" =>
           val anyValClassDefns = paramss.flatten.map(anyValClassForField(tname, _))
-          val newSource = source"..${anyValClassDefns.map(_.defn)}"
-          println(newSource.syntax)
+          val anyValClassesSource = source"..${anyValClassDefns.map(_.defn)}"
+          println(anyValClassesSource.syntax)
+          val replacedTypesFields = anyValClassDefns.map { case ClassDefnWithName(className, _, fieldName) =>
+            Term.Param(List.empty[Mod], fieldName, Some(className), None)
+          }
+          val replacedTypesClassSource = source"case class $tname(...${List(replacedTypesFields)})"
+          println(replacedTypesClassSource.syntax)
         case _ =>
           println("not a class")
       }
