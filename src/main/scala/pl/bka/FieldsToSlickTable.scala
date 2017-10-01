74e535e4d51db3a12331e44e11a551f34052cdf3
Bartek Kalinka <bartkali@hotmail.com>
2017-10-01 22:15:01 +0200

    slick table draft

---
+++ src/main/scala/pl/bka/FieldsToSlickTable.scala
@@ -0,0 +1,24 @@
+package pl.bka
+
+import scala.meta._
+
+object FieldsToSlickTable {
+  case class SlickTableOutput(slickTable: String)
+
+  def generate(input: String): Either[String, SlickTableOutput] = {
+    val tree = input.parse[Source].get
+    tree match {
+      case source"..$stats" =>
+        stats.head match {
+          case q"..$mods class $tname[..$tparams] ..$ctorMods (...$paramss) extends $template" =>
+            val tableClassName = Type.Name(s"${tname}s")
+            val initsArgs = List(List(q"tag", Lit.String(tableClassName.syntax)))
+            val outputSource = source"case class $tableClassName(tag: BaseTable.Tag) extends BaseTable[$tname](...$initsArgs)"
+            Right(SlickTableOutput(outputSource.syntax))
+          case _ =>
+            Left("not a class")
+        }
+    }
+  }
+}
+
--- src/main/scala/pl/bka/Main.scala
+++ src/main/scala/pl/bka/Main.scala
@@ -1,6 +1,7 @@
 package pl.bka
 
 import pl.bka.FieldsToAnyValCaseClasses.AnyValCaseClassesOutput
+import pl.bka.FieldsToSlickTable.SlickTableOutput
 
 object Main extends App {
   val input =
@@ -15,10 +16,16 @@ object Main extends App {
       |)
     """.stripMargin
 
-  FieldsToAnyValCaseClasses.generate(input) match {
-    case Right(AnyValCaseClassesOutput(anyValClasses, replacedTypesClass)) =>
-      println(anyValClasses)
-      println(replacedTypesClass)
+//  FieldsToAnyValCaseClasses.generate(input) match {
+//    case Right(AnyValCaseClassesOutput(anyValClasses, replacedTypesClass)) =>
+//      println(anyValClasses)
+//      println(replacedTypesClass)
+//    case Left(error) => println(s"error $error")
+//  }
+
+  FieldsToSlickTable.generate(input) match {
+    case Right(SlickTableOutput(slickTable)) =>
+      println(slickTable)
     case Left(error) => println(s"error $error")
   }
 }
